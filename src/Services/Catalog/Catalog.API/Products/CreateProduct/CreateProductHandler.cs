85c40061a20909edeba3de9a149aabb591419581
Ashish Kadam <ashishkadam7262@gmail.com>
2025-04-28 17:24:48 +0530

    Common library BuildingBlocks created where all the common required packages will be stored. This library will be referenced by all microservices. CreateProduct Service folder created. Yet to be completely implemented

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,17 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResponse>;
+
+public record CreateProductResponse(Guid Id);
+internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResponse>
+{
+    public Task<CreateProductResponse> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        // create Product entity from command object
+        // save to database
+        // return CreateProductResponse result
+        throw new NotImplementedException();
+    }
+}
